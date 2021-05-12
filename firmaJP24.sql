f4d1f3fafe2985b76bdfaa0e9e389710c33d9b91
Petar Jagrić <petarjagric@gmail.com>
2021-05-12 17:43:27 +0200

    first table

--- firmaJP24.sql
+++ firmaJP24.sql
@@ -2,4 +2,8 @@ drop database if exists firmaJP24;
 create database firmaJP24;
 use firmaJP24;
 
-create table
+create table projekt(
+sifra int not null primary key auto_increment,
+naziv varchar(50) not null,
+cijena decimal(18,2) null
+);
